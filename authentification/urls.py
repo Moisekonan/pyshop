29c56083366a86651434c5a9aef5f595ac8e9448
KonanMoise <bjrmoise06@icloud.com>
2023-06-29 12:49:47 +0000

    les template et vues basique de authentification OK

--- authentification/urls.py
+++ authentification/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='auth_login'),
+    path('register', views.register, name='auth_register'),
+    path('motdepasse_oublier', views.motdepasse_oublier, name='auth_motdepasse_oublier'),
+    path('user_profile', views.user_profil, name='auth_user_profile'),
 ]
--- authentification/views.py
+++ authentification/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def login(request):
     return render(request, 'authentification/login.html')
+
+def register(request):
+    return render(request, 'authentification/register.html')
+
+def motdepasse_oublier(request):
+    return render(request, 'authentification/forgot-password.html')
+
+def user_profil(request):
+    return render(request, 'authentification/user-profile.html')
